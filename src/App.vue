666a494fee6a58e47ee52dfe6fcbf922e448ad36
cyrilpluche <cyril@vauban.io>
2019-12-13 19:36:21 +0000

    project cleaning

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    {{ people }}
   </div>
 </template>
 
@@ -20,9 +19,7 @@ export default {
     HelloWorld
   },
   mounted() {
-    getCharacter().then(people => {
-      this.people = people
-    })
+    
   }
 }
 </script>
