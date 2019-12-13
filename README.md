593f7ef6696f1d61a42622633326620ccef5b39f
cyrilpluche <cyril@vauban.io>
2019-12-13 19:34:29 +0000

    project guidelines

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# vauban-front-test
+# VAUBAN Front End Test
 
-Front End technical test
+## Guidelines
+
+You will build an application that allow an user to search through a list of Star Wars characters and add them to his personal list.
+
+- Display a list of characters into nice cards with their informations
+- Implement a search function that filter the list of characters
+- Display the list of selected characters by the user
+
+You will find the list of characters into **data/characters.js** file. 
+
+
+You can add any other external packages if you need.
+
+We will pay intention to the way you organize your project and how you handle interactions inside your application.
---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ people }}
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      people: null
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    getCharacter().then(people => {
+      this.people = people
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
